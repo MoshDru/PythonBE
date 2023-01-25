d6c45ab06356743bc2f0c433f4ecfa454add4d92
Moshe Druion <moshedruion@Moshes-MacBook-Pro.local>
2023-01-25 09:14:21 +0200

    first commit

---
+++ sumofdigit.py
@@ -0,0 +1,14 @@
+print("Hello world")
+
+
+# Function to check sum of
+# digit using recursion
+def sum_of_digit( n ):
+    if n == 0:
+        return 0
+    return (n % 10 + sum_of_digit(int(n / 10)))
+ 
+# Driven code to check above
+num = 2347623
+result = sum_of_digit(num)
+print("Sum of digits in",num,"is", result)
